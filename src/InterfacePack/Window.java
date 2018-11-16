8cefd7ee8545243b152557064426ef6c47ec58cb
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-16 19:19:44 +0300

    Refactor #3

--- src/InterfacePack/Window.java
+++ src/InterfacePack/Window.java
@@ -2,6 +2,8 @@ package InterfacePack;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 
 public class Window extends Canvas
@@ -20,21 +22,23 @@ public class Window extends Canvas
     private void renderBuffer()
     {
         if(buffer == null)
-            createBufferStrategy(6);
+            createBufferStrategy(3);
 
         buffer = getBufferStrategy();
         graphics = buffer.getDrawGraphics();
         graphics.fillRect(0, 0, getWidth(), getHeight());
         graphics.setColor(Color.WHITE);
         graphics.dispose();
+        buffer.show();
     }
 
     private void initWindow(String name)
     {
+        frame = new JFrame(name);
         frame.setResizable(true);
-        frame.add(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setTitle(name);
+        frame.add(this);
         frame.pack();
     }
 
