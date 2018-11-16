2c7e01326aa6b05e5d2bd4137524af2391885a61
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-16 23:53:51 +0300

    Create test server

--- src/ServerPack/Server.java
+++ src/ServerPack/Server.java
@@ -1,6 +1,31 @@
 package ServerPack;
 
-public class Server extends Thread
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
 {
-    //TODO implementation server
+    ServerSocket serverSocket;
+    Socket client;
+    int countClient = 0;
+
+
+    public void addClient(int countClient)
+    {
+        try {
+            serverSocket = new ServerSocket(8493);
+            client = serverSocket.accept();
+            while (!client.isClosed())
+            {
+                if(client.isConnected())
+                {
+                    this.countClient = countClient;
+                    countClient++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
