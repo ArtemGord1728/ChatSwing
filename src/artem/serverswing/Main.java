f31d4687194c51bcf7054537539d873fbd5eb066
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-16 23:21:38 +0300

    Changes

--- src/artem/serverswing/Main.java
+++ src/artem/serverswing/Main.java
@@ -2,12 +2,10 @@ package artem.serverswing;
 
 import InterfacePack.Window;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args) {
-        Window window = new Window("Server", 400, 400);
+        Window window = new Window("ServerPack", 400, 400);
         window.show();
     }
 }
