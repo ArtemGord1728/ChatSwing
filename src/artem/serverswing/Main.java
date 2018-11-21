091e7d2756e371a208a94e051ef9f0c4fd807c17
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-21 15:47:40 +0300

    Changed size of window

--- src/artem/serverswing/Main.java
+++ src/artem/serverswing/Main.java
@@ -5,6 +5,6 @@ import InterfacePack.Window;
 public class Main {
 
     public static void main(String[] args) {
-        new Window("ServerPack", 400, 400);
+        new Window("ServerPack", 500, 500);
     }
 }
