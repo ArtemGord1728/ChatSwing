3963b59686c5510b23607c3ea73e3a359da17fd4
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-23 03:14:21 +0300

    Changes

--- src/ServerPack/Server.java
+++ src/ServerPack/Server.java
@@ -1,42 +1,20 @@
 package ServerPack;
 
-import InterfacePack.Window;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class Server extends Thread {
-    ServerSocket serverSocket;
-    private Socket client;
-    DataInputStream is;
-    DataOutputStream os;
-    public static boolean isEntryToServer;
-
-    @Override
-    public void run() {
-        runServer(Window.textArea);
-    }
+public class Server
+{
+    private Thread serverRun;
+    private int client;
+    private boolean isConnect = false;
 
 
+    public boolean actionServer(int client){
+        serverRun = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-    public void runServer(JTextArea jTextArea) {
-        try {
-            serverSocket = new ServerSocket(8493);
-            client = serverSocket.accept();
-            is = new DataInputStream(client.getInputStream());
-            os = new DataOutputStream(client.getOutputStream());
-            os.flush();
-
-            String message = is.readUTF();
-            jTextArea.setText(message);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            }
+        });
+        return true;
     }
 }
