f257547e96d65cca99dee2901a43829f23e260db
ArtemGord1728 <artemseraf01@mail.ru>
2018-11-27 00:12:24 +0300

    Changes

--- src/ServerPack/Server.java
+++ src/ServerPack/Server.java
@@ -1,24 +1,49 @@
 package ServerPack;
 
+import ServerClientsFunctionality.ServerFunctionality;
 
-public class Server
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Date;
+
+public class Server implements ServerFunctionality
 {
-    private Thread serverRun;
-    private int client;
-    private boolean isConnect = false;
+    DatagramPacket packetReceive;
+    DatagramSocket socket;
+    int port;
+    Thread receiveMessages;
 
-    public Server()
-    {
-        System.out.println("Server start!");
+    public Server(int port) {
+        this.port = port;
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
 
-    public boolean actionServer(int client){
-        serverRun = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
+    @Override
+    public void receive() {
+        receiveMessages = new Thread(() -> {
+            byte[] mess = new byte[1024];
+            packetReceive = new DatagramPacket(mess, mess.length);
+            try {
+                socket.receive(packetReceive);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
-        return true;
+    }
+
+    @Override
+    public void gettingMessage() {
+
+    }
+
+    @Override
+    public void structMessage(Date date, String message) {
+        System.out.print(date.getTime() + " " + message);
     }
 }
