329cf1e0caa5284105e40a95d9d2b59e22781152
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-09 21:30:55 +0300

    Changes

--- src/Windows/AuthorizationWindow.java
+++ src/Windows/AuthorizationWindow.java
@@ -6,14 +6,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
-public class AutorizeWindow extends Canvas implements Layer
+public class AuthorizationWindow extends Canvas implements Layer
 {
     private BufferStrategy buffer;
     private JFrame frame;
     private JLabel login, host, password;
     private Graphics graphics;
 
-    public AutorizeWindow(String name, int width, int height)
+    public AuthorizationWindow(String name, int width, int height)
     {
         setPreferredSize(new Dimension(width, height));
         initWindow(name);
@@ -28,7 +28,7 @@ public class AutorizeWindow extends Canvas implements Layer
         frame.add(loginLabel);
 
         JLabel hostLabel = new JLabel("Host");
-        hostLabel.setBounds(83, 30, 180, 200);
+        hostLabel.setBounds(83, 25, 180, 200);
         frame.add(hostLabel);
 
         JLabel ageLabel = new JLabel("Age");
@@ -40,13 +40,13 @@ public class AutorizeWindow extends Canvas implements Layer
     public void showTextLayer() {
         JTextField loginInput = new JTextField();
         loginInput.setVisible(true);
-        loginInput.setBounds(50, 80, 180, 200);
+        loginInput.setBounds(50, 70, 180, 200);
         loginInput.setSize(new Dimension(100, 30));
         frame.add(loginInput);
 
         JTextField hostInput = new JTextField();
         hostInput.setVisible(true);
-        hostInput.setBounds(50, 140, 180, 200);
+        hostInput.setBounds(50, 135, 180, 200);
         hostInput.setSize(new Dimension(100, 30));
         frame.add(hostInput);
 
