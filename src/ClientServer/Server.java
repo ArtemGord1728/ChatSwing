58fd52bacb03e9ee9121645642833901e4fc9d87
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-09 20:52:24 +0300

    Changes

--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -1,4 +1,39 @@
 package ClientServer;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    ServerSocket server;
+    int port;
+
+    public Server(int port) {
+        this.port = port;
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+
+        }
+    }
+
+    public boolean connect()
+    {
+        try {
+            Socket socket = server.accept();
+
+            if(socket.isConnected())
+                return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
 }
