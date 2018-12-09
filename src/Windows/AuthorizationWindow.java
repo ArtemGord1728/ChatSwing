bc149f63f869223b1219713db7b0ce3eb5b49d59
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-09 21:18:58 +0300

    Refacctor #5

--- src/Windows/AuthorizationWindow.java
+++ src/Windows/AuthorizationWindow.java
@@ -24,13 +24,37 @@ public class AutorizeWindow extends Canvas implements Layer
     @Override
     public void showLabels() {
         JLabel loginLabel = new JLabel("Login");
-        loginLabel.setBounds(80, -20, 180, 200);
+        loginLabel.setBounds(80, -40, 180, 200);
         frame.add(loginLabel);
+
+        JLabel hostLabel = new JLabel("Host");
+        hostLabel.setBounds(83, 30, 180, 200);
+        frame.add(hostLabel);
+
+        JLabel ageLabel = new JLabel("Age");
+        ageLabel.setBounds(85, 90, 180, 200);
+        frame.add(ageLabel);
     }
 
     @Override
     public void showTextLayer() {
+        JTextField loginInput = new JTextField();
+        loginInput.setVisible(true);
+        loginInput.setBounds(50, 80, 180, 200);
+        loginInput.setSize(new Dimension(100, 30));
+        frame.add(loginInput);
 
+        JTextField hostInput = new JTextField();
+        hostInput.setVisible(true);
+        hostInput.setBounds(50, 140, 180, 200);
+        hostInput.setSize(new Dimension(100, 30));
+        frame.add(hostInput);
+
+        JTextField ageInput = new JTextField();
+        ageInput.setVisible(true);
+        ageInput.setBounds(50, 200, 180, 200);
+        ageInput.setSize(new Dimension(100, 30));
+        frame.add(ageInput);
     }
 
     @Override
