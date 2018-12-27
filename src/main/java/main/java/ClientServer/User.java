4eea442054bfec2568ea6c9f2ef20aee4b9a5d1b
ArtemGord1728 <artemseraf01@mail.ru>
2018-12-28 00:29:39 +0300

    Added new file

---
+++ src/main/java/main/java/ClientServer/User.java
@@ -0,0 +1,14 @@
+package main.java.ClientServer;
+
+public class User
+{
+    private String name;
+    private int age;
+    private String host;
+
+    public User(String name, int age, String host) {
+        this.name = name;
+        this.age = age;
+        this.host = host;
+    }
+}
