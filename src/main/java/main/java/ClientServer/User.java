ef02794e0debc7831179eee85589a4da677c6bf4
ArtemGord1728 <artemseraf01@mail.ru>
2019-01-15 02:32:34 +0300

    Changes

--- src/main/java/main/java/ClientServer/User.java
+++ src/main/java/main/java/ClientServer/User.java
@@ -3,21 +3,15 @@ package main.java.ClientServer;
 public class User
 {
     private String name;
-    private int age;
     private String host;
 
-    public User(String name, int age, String host) {
+    public User(String name, String host) {
         this.name = name;
-        this.age = age;
         this.host = host;
     }
 
     public String getName() {
         return name;
-    }
-
-    public int getAge() {
-        return age;
     }
 
     public String getHost() {
