2610f2ba6312c407ed8422de5c5d5960184a6482
ArtemGord1728 <artemseraf01@mail.ru>
2019-01-15 02:33:52 +0300

    Changes

--- src/main/java/main/java/artem/serverswing/Main.java
+++ src/main/java/main/java/artem/serverswing/Main.java
@@ -6,11 +6,6 @@ import main.java.Windows.ClientWindow;
 
 public class Main {
     public static void main(String[] args) {
-        new AuthorizationWindow("Auth", 200, 400);
-
-        if(AuthorizationWindow.isRegistration)
-        {
-            new ClientWindow("Client", 500, 500);
-        }
+        new AuthorizationWindow("Auth", 200, 300);
     }
 }
