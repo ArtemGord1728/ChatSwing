5ab64bdd570cc9af20ab13100b5d7990316d2fb5
ArtemGord1728 <artemseraf01@mail.ru>
2019-01-15 02:34:49 +0300

    refactor

--- src/main/java/main/java/ClientServer/ServerSide.java
+++ src/main/java/main/java/ClientServer/ServerSide.java
@@ -5,7 +5,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server
+public class ServerSide
 {
     private ServerSocket server;
     private InetAddress ip;
@@ -13,34 +13,28 @@ public class Server
     private Socket socket;
     private Thread runServer;
 
-    public Server(int port, String host) {
+    public ServerSide(int port, String host) {
         this.port = port;
         try {
             server = new ServerSocket(port);
             ip = InetAddress.getByName(host);
-            socket = new Socket(host, port);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        while (true) {
-            activateServer();
-        }
+        activateServer();
     }
 
     private void activateServer() {
         runServer = new Thread(new Runnable() {
             @Override
             public void run() {
-
+                try {
+                    socket = server.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
-    }
-
-    public boolean isClientConnection() {
-        if(socket.isConnected())
-            return true;
-        else
-            return false;
     }
 }
