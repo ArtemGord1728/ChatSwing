45fc58d03b7bf63819064eca770d8bb2aeda27ba
ArtemGord1728 <artemseraf01@mail.ru>
2019-02-03 19:48:08 +0300

    Some changes

--- src/main/java/main/java/ClientServer/ServerSide.java
+++ src/main/java/main/java/ClientServer/ServerSide.java
@@ -2,24 +2,30 @@ package main.java.ClientServer;
 
 
 import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ServerSide {
+public class ServerSide
+{
     private Socket socket;
     private ServerSocket server;
     private InetAddress ip;
+    private static final int MAX_CLIENTS_ON_SERVER = 10;
     private int port;
     private Thread runServer;
+    private DatagramSocket datagramSocket;
+    private ExecutorService executorServer;
+    private DatagramPacket datagramPacket;
 
     public ServerSide(int port, @NotNull String host) {
         this.port = port;
         try {
             server = new ServerSocket(port);
             ip = InetAddress.getByName(host);
+            datagramSocket = new DatagramSocket(port);
+            executorServer = Executors.newFixedThreadPool(MAX_CLIENTS_ON_SERVER);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,5 +44,14 @@ public class ServerSide {
             }
         }, "runServer");
         runServer.start();
+    }
+
+    private void waitingForData(byte[] data){
+        datagramPacket = new DatagramPacket(data, data.length);
+        try {
+            datagramSocket.receive(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
