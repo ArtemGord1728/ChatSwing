b7bd43e3928b3fb6f931832bdc44a3be4488cde3
ArtemGord1728 <artemseraf01@mail.ru>
2019-02-03 20:29:02 +0300

    Some changes

--- src/main/java/main/java/ClientServer/ClientSide.java
+++ src/main/java/main/java/ClientServer/ClientSide.java
@@ -1,22 +1,45 @@
 package main.java.ClientServer;
 
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
-public class ClientSide {
+public class ClientSide extends Thread {
     private Socket clientSocket;
+    private InetAddress address;
     private int port;
     private DatagramPacket packet;
     private DatagramSocket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
 
     public ClientSide(int port) {
-        clientSocket = new Socket();
+        try {
+            clientSocket = new Socket("localhost", port);
+            address = InetAddress.getLocalHost();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.port = port;
+        start();
     }
 
-    public void sendMessage(byte[] message) {
+    public void sendTextMessage(byte[] message) {
+        packet = new DatagramPacket(message, message.length, address, port);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendFileMessage(byte[] message, File file){
 
     }
 }
