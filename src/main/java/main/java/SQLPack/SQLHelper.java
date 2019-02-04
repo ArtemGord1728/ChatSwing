c5d7dbcc791011379b1fe7c21061a8d16372665b
ArtemGord1728 <artemseraf01@mail.ru>
2019-02-04 22:45:44 +0300

    Minor changes

--- src/main/java/main/java/SQLPack/SQLHelper.java
+++ src/main/java/main/java/SQLPack/SQLHelper.java
@@ -1,4 +1,17 @@
 package main.java.SQLPack;
 
-public class SQLHelper {
+import java.sql.Connection;
+
+public class SQLHelper
+{
+    Connection connection;
+
+    private SQLHelper()
+    {
+
+    }
+
+    public static SQLHelper getInstanceSQL(){
+        return new SQLHelper();
+    }
 }
