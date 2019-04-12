6d2edf677e556acfd0a8bd4ddfba02be198398c2
ArtemGord1728 <artemseraf01@mail.ru>
2019-04-13 02:00:53 +0300

    'Refactor #5

--- src/main/java/main/java/artem/serverswing/ServerMain.java
+++ src/main/java/main/java/artem/serverswing/ServerMain.java
@@ -1,38 +1,28 @@
 package main.java.artem.serverswing;
 
-import main.java.AppManager.Manager;
 import main.java.CoreApp.ServerSide;
 import main.java.Windows.AuthorizationWindow;
-import main.java.Windows.Client;
-
+import java.net.UnknownHostException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-public class Main {
+public class ServerMain {
 
-    private int port;
-    private ServerSide serverSide;
+	private int port;
 
-    public Main(int port){
-        this.port = port;
-        new ServerSide(port);
-    }
+	public ServerMain(int port) {
+		this.port = port;
+		new ServerSide(port);
+	}
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        int port;
-        if(args.length != 1) {
-            System.out.println("Usage: java -jar program.jar <port>");
-            System.exit(1);
-        }
-        if(args[0].equalsIgnoreCase("/exit")){
-            System.out.println("Exit from server!");
-            System.exit(1);
-        }
-        port = Integer.parseInt(args[0]);
-        new ServerSide(port);
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, UnknownHostException {
+		int port;
+		String str;
 
-//        new AuthorizationWindow("name", 300, 300);
-//        new Manager();
+		Scanner sc = new Scanner(System.in);
+		port = sc.nextInt();
+		new ServerMain(port);
 
-
-    }
+		
+	}
 }
