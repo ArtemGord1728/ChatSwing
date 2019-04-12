84c4b3f7017098ba7e65d86a02ee8e757133b10d
ArtemGord1728 <artemseraf01@mail.ru>
2019-04-13 01:33:49 +0300

    Added new directory for checking user in DB

---
+++ src/main/java/main/java/VerifierUtil/VerifierGUI.java
@@ -0,0 +1,24 @@
+package main.java.VerifierUtil;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+
+import main.java.InterfacePack.Layer;
+
+public class VerifierGUI extends JFrame
+{
+	private JProgressBar progress;
+	private JLabel label;
+	
+	public VerifierGUI() {
+		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		
+	}
+}
