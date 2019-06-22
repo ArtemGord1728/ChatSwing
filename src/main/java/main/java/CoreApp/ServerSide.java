8edffafb54d2bd4fd1557c1006a63f404c1632d7
ArtemGord1728 <artemseraf01@mail.ru>
2019-06-23 02:46:21 +0300

    some changes

--- src/main/java/main/java/CoreApp/ServerSide.java
+++ src/main/java/main/java/CoreApp/ServerSide.java
@@ -3,9 +3,12 @@ package main.java.CoreApp;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerSide
 {
@@ -58,5 +61,8 @@ public class ServerSide
             }
         }, "receiveData");
         receiveData.start();
+    }
+    void addNewUser() {
+    	clients = new ArrayList();
     }
 }
