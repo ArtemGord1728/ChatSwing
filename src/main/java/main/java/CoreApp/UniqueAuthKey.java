f0a3899b496431b4e131cfe8077ab506117d0bad
ArtemGord1728 <artemgord00@gmail.com>
2019-07-17 20:55:48 +0300

    changes

--- src/main/java/main/java/CoreApp/UniqueAuthKey.java
+++ src/main/java/main/java/CoreApp/UniqueAuthKey.java
@@ -6,10 +6,6 @@ public class UniqueAuthKey {
     private UniqueAuthKey() {
     }
 
-    public UniqueAuthKey getAuthKey() {
-    	return new UniqueAuthKey();
-    }
-    
     public static UUID setAuthKey() {
         byte[] arrayByte = {10, 20};
         return UUID.nameUUIDFromBytes(arrayByte);
