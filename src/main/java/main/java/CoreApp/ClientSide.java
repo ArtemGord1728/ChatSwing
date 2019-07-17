f008bbe7db0f7cf54573ca6e5f8021a9f285a065
ArtemGord1728 <artemgord00@gmail.com>
2019-07-17 04:20:41 +0300

    Minor changes

--- src/main/java/main/java/CoreApp/ClientSide.java
+++ src/main/java/main/java/CoreApp/ClientSide.java
@@ -18,28 +18,37 @@ public class ClientSide {
     	this.port = port;
     	this.name = name;
     	this.host = host;
-        try {
+    	
+    	connectToIpAndSocket();
+    }
+    
+    public ClientSide(int port, String host) {
+    	this.port = port;
+    	this.host = host;
+    	
+		connectToIpAndSocket();
+	}
+    
+    private void connectToIpAndSocket() {
+    	try {
+    		socket = new DatagramSocket();
         	ip = InetAddress.getByName(host);
         	socket = new DatagramSocket(port);
-        	
         } catch (IOException e) {
             e.printStackTrace();
-        }        
+        } 
     }
-
-    public String sendTextMessage(byte[] message) {
+    
+    public void sendTextMessage(byte[] message) {
         packet = new DatagramPacket(message, message.length, ip, port);
         try {
             socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
-            return "";
         }
-
-        return new String(packet.getData());
     }
 
-    public void sendFileMessage(byte[] message, String fileName){
+    public void sendFileMessage(byte[] message, String fileName) {
         try {
             FileInputStream fileStream = new FileInputStream(fileName);
             while (fileStream.read() != -1) {
@@ -51,4 +60,16 @@ public class ClientSide {
             e.printStackTrace();
         }
     }
+    
+    public String getHost() {
+		return host;
+	}
+    
+    public String getName() {
+		return name;
+	}
+    
+    public int getPort() {
+		return port;
+	}
 }
