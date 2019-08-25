d7476678efb18777b95713d218f7fd317eaeb6ed
ArtemGord1728 <artemgord00@gmail.com>
2019-08-25 23:28:32 +0300

    refactor

--- src/main/java/main/java/core/ServerMain.java
+++ src/main/java/main/java/core/ServerMain.java
@@ -1,18 +1,13 @@
 package main.java.core;
 
+import java.util.Scanner;
 
 public class ServerMain {
 	public static void main(String[] args) {
 		int port;
-		String host;
-
-		if(args.length == 0) {
-			System.out.println("Usage: java -jar Server.jar <port> <host>");
-		}
 		
-		port = Integer.parseInt(args[0]);
-		host = args[1];
-		
-		new ServerSide(port, host);
+		Scanner sc = new Scanner(System.in);
+		port = sc.nextInt();
+		new ServerSide(port);
 	}
 }
